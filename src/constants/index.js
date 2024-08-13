5be4392aac2b8ae034b733320befd16c526c0ade
Sungjun Hyeon <wnsgus880@naver.com>
2024-08-13 18:04:41 +0900

    1차 완성

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 import port01 from "../assets/img/port01.jpg";
 import port02 from "../assets/img/port02.jpg";
 import port03 from "../assets/img/port03.jpg";
@@ -11,7 +9,97 @@ import port08 from "../assets/img/port08.jpg";
 import port09 from "../assets/img/port09.jpg";
 import port10 from "../assets/img/port10.jpg";
 
-const portText = [
+export  const headerNav = [
+    {
+        title: "intro",
+        url: "#intro"
+    },
+    {
+        title: "skill",
+        url: "#skill"
+    },
+    {
+        title: "site",
+        url: "#site"
+    },
+    {
+        title: "portfolio",
+        url: "#port"
+    },
+    {
+        title: "contact",
+        url: "#contact"
+    }
+];
+
+export const introText = {
+    title: "port developer",
+    desc: ["talent is", "found at the end of the", "effort"]
+}
+
+export const skillText = [
+    {
+        title: "꿈을 설계하고 디자인하다.",
+        desc: "나는 공간을 만드는 것을 좋아한다. 어려을 때부터 나만의 공간을 만드는 것을 좋아했고 나만의 다락방을 좋아했다. 단 한 사람이라도 내가 만든 공간 속에서 영감을 받거나 마음이 움직였으면 좋겠다. 나만의 공간을 마음것 만들 수 있다는 건 코딩에 엄청난 매력인거 같다. 그 한구석에 나만의 꿈을 설계하고, 개발을 하며 앞으로도 살고 싶다."
+    },
+    {
+        title: "열심히 할수록 기회는 따른다.",
+        desc: "운이 좋은 사람은 없다. 단지 운을 만들 뿐이다. 운을 만들기 위해서는 내가 좋아하는 일이나 내가 하고 싶은 일에 몰두하면 된다. 몰두 하다보면 길이 보이고 방향이 보이게 된다. 운이란 고된 노동과 노력을 통해 스스로 만들어 내는 것이다."
+    },
+    {
+        title: "나에게 정직하다.",
+        desc: "정직은 다른 사람보다 나에게 큰 의미를 부여해야 한다. 자신이 정직하지 않으면 진정으로 원하는 일을 열정적으로 밀고 나갈 수 없다. 마음에서 우러나오는 일을 해야 정직해지며 삶을 더 즐길 줄 알게 된다."
+    }
+]
+
+export const siteText = [
+    {
+        text: ["make", "site compliant with", "webstandard"],
+        title: "비트를 이용한 사이트 제작",
+        code: "https://github.com/webstoryboy/port2023-vite",
+        view: "https://port2023-vite.netlify.app",
+        info: [
+            "site coding",
+            "production period : two days",
+            "use stack : HTML5/CSS3, CSS Variable, Vite",
+        ],
+    },
+    {
+        text: ["make", "site compliant with", "react.js"],
+        title: "리액트를 이용한 사이트 제작",
+        code: "https://github.com/webstoryboy/port2023-react",
+        view: "https://port2023-react.netlify.app",
+        info: [
+            "site coding",
+            "production period : two days",
+            "use stack : HTML5/CSS3, CSS Variable, react",
+        ],
+    },
+    {
+        text: ["make", "site compliant with", "vue.js"],
+        title: "뷰를 이용한 사이트 제작",
+        code: "https://github.com/webstoryboy/port2023-vue",
+        view: "https://port2023-vue.netlify.app",
+        info: [
+            "site coding",
+            "production period : two days",
+            "use stack : HTML5/CSS3, Scss Variable, vue",
+        ],
+    },
+    {
+        text: ["make", "site compliant with", "next.js"],
+        title: "넥스트를 이용한 사이트 제작",
+        code: "https://github.com/webstoryboy/port2023-next",
+        view: "https://port2023-next.netlify.app",
+        info: [
+            "site coding",
+            "production period : two days",
+            "use stack : HTML5/CSS3, Scss Variable, next.js",
+        ],
+    },
+];
+
+export const portText = [
     {
         num: "01",
         title: "어워드에도 올라간 포트폴리오",
@@ -104,29 +192,52 @@ const portText = [
     },
 ];
 
-const Port = () => {
-  return (
-      <section id="port">
-          <div className="port__inner">
-              <div className="port__title">
-                  portfolio <em>포폴 작업물</em>
-              </div>
-              <div className="port__wrap">
-                  {portText.map((port, key) => (
-                      <article className={`port__item p${key + 1}`} key={key}>
-                          <span className="num">{port.num}.</span>
-                          <a href={port.code} target="_blank" className="img" rel="noreferrer">
-                              <img src={port.img} alt={port.name} />
-                          </a>
-                          <h3 className="title">{port.title}</h3>
-                          <p className="desc">{port.desc}</p>
-                          <a href={port.view} target="_blank" className="site" rel="noreferrer">사이트 보기</a>
-                      </article>
-                  ))}
-              </div>
-          </div>
-      </section>
-  );
-};
+export const contactText = [
+    {
+        link: "https://open.kakao.com/o/gM7YLzwf",
+        title: "KAKAO : webstupids",
+    },
+    {
+        link: "mailto:webstoryboy@naver.com",
+        title: "mail : webstoryboy@naver.com",
+    },
+];
 
-export default Port;
+export const footerText = [
+    {
+        title: "youtube",
+        desc: "유튜브에 오시면 더 많은 강의를 볼 수 있습니다.",
+        link: "https://www.youtube.com/@Webstoryboy",
+    },
+    {
+        title: "github",
+        desc: "깃헙에 오시면 더 많은 소스를 볼 수 있습니다.",
+        link: "https://github.com/webstoryboy",
+    },
+    {
+        title: "blog",
+        desc: "블러그에 오시면 더 많은 정보를 볼 수 있습니다.",
+        link: "https://webstoryboy.co.kr",
+    },
+    {
+        title: "gsap",
+        desc: "GSAP에 오시면 더 많은 강의를 볼 수 있습니다.",
+        link: "https://www.youtube.com/playlist?list=PL4UVBBIc6giL8-6jvrClimg0cFL-Muqiq",
+    },
+    {
+        title: "react",
+        desc: "리액트로 만든 사이트를 같이 만들어 봅니다.",
+        link: "https://github.com/webstoryboy/port2023-react",
+    },
+    {
+        title: "vue",
+        desc: "뷰로 만든 사이트를 같이 만들어 봅니다.",
+        link: "https://github.com/webstoryboy/port2023-vue",
+    },
+    {
+        title: "next",
+        desc: "넥스트로 만든 사이트를 같이 만들어 봅니다.",
+        link: "https://github.com/webstoryboy/port2023-next",
+    },
+];
+
