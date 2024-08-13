b36ce866c783486a004f69c427e0c6f128d63775
Sungjun Hyeon <wnsgus880@naver.com>
2024-08-13 12:36:26 +0900

    🥳 헤더영역 완료

--- src/components/Skip.jsx
+++ src/components/Skip.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Skip = () => {
   return (
-    <div>Skip</div>
+    <div id='skip'>
+      <a href="/header">header</a>
+      <a href="/intro">intro</a>
+      <a href="/skill">skill</a>
+      <a href="/site">site</a>
+      <a href="/port">portfolio</a>
+      <a href="/contact">contact</a>
+      <a href="/footer">footer</a>
+    </div>
+
   )
 }
 
